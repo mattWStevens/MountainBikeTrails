0d758c859fee156434467e628bb69c1fa0d86265
mattWStevens <gibsonhistoric@gmail.com>
2020-11-07 14:59:10 -0700

    Created Trail model class, ran initial migrations, and registered it in the admin.

--- mountainbiketrails/trails/models.py
+++ mountainbiketrails/trails/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Trail(models.Model):
+    name = models.CharField(max_length=200)
+    length = models.DecimalField(default=0.1, decimal_places=1, max_digits=3)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
+    visited = models.BooleanField(default=False)
+    visited_on = models.DateField()
+    difficulty_level = models.IntegerField(default=0)
+    my_rating = models.IntegerField(default=0)
+    top_ten = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Name: {self.name}, Location: {self.city}, {self.state}, {self.country}\nLength: {self.length}"
