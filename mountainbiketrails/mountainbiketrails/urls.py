7511a91e069f44da2fea2bdfb405e0819db980d8
mattWStevens <gibsonhistoric@gmail.com>
2020-11-07 15:12:43 -0700

    Added initial HTML template for the home page of the webapp.

--- mountainbiketrails/mountainbiketrails/urls.py
+++ mountainbiketrails/mountainbiketrails/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from webapp.views import welcome
+from webapp.views import home_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', welcome)
+    path('', home_page)
 ]
