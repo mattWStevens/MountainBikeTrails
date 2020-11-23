a2966312f1d3d33565ac74f1a71ff720ce5c807e
mattWStevens <gibsonhistoric@gmail.com>
2020-11-23 15:45:30 -0700

    Added in initial template for add new trail page, added more styling to main page, and added in initial view function for new trail page.

--- mountainbiketrails/webapp/urls.py
+++ mountainbiketrails/webapp/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from .views import home_page
 
 urlpatterns = [
     path('', home_page, name="home"),
+    path('add', include('trails.urls'))
 ]
