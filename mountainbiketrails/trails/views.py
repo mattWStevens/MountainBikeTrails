a8e9128e9610263d0ceef3b424abea649b5b4bc4
mattWStevens <gibsonhistoric@gmail.com>
2020-11-24 16:44:37 -0700

    Added in initial form using Django's form functionality and also added url navigation to/from the add view.

--- mountainbiketrails/trails/views.py
+++ mountainbiketrails/trails/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.forms import modelform_factory
 from .models import Trail
 
+TrailForm = modelform_factory(Trail, exclude=[])
+
 def add(request):
-    return render(request, "trails/add.html")
+    if request.method == "POST":
+        form = TrailForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+
+    else:
+        form = TrailForm()
+
+    return render(request, "trails/add.html", {"form": form})
 
 def detail(request, id):
     trail = get_object_or_404(Trail, pk=id)
