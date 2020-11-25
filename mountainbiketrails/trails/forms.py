3cae44624b5e1d13aae9de9ca2e3e04b272c3b29
mattWStevens <gibsonhistoric@gmail.com>
2020-11-24 17:28:58 -0700

    Added in custom form class with widgets.

---
+++ mountainbiketrails/trails/forms.py
@@ -0,0 +1,22 @@
+from django.forms import ModelForm, TextInput, SelectDateWidget
+from django.core.exceptions import ValidationError
+from .models import Trail
+
+
+class TrailForm(ModelForm):
+    class Meta:
+        model = Trail
+        fields = '__all__'
+        widgets = {
+            'visited_on': SelectDateWidget,
+            'difficulty_level': TextInput(attrs={"type": "number", "min":"0", "max": "10"}),
+            'my_rating': TextInput(attrs={"type": "number", "min": "0", "max": "5"})
+        }
+
+    def clean_length(self):
+        l = self.cleaned_data.get("length")
+
+        if l < 0:
+            raise ValidationError("Trail cannot be a negative length.")
+
+        return l
--- mountainbiketrails/trails/views.py
+++ mountainbiketrails/trails/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.forms import modelform_factory
 from .models import Trail
-
-TrailForm = modelform_factory(Trail, exclude=[])
+from .forms import TrailForm
 
 def add(request):
     if request.method == "POST":
