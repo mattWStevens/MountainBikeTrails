302e9cca666c944e0d4f31877a59aa5c037e9c69
mattWStevens <gibsonhistoric@gmail.com>
2020-12-13 15:29:40 -0700

    Added more JavaScript to handle automatically showing/not showing the properties related to the visited checkbox depending on if it is selected or not.

--- mountainbiketrails/webapp/static/js/forms.js
+++ mountainbiketrails/webapp/static/js/forms.js
@@ -1,22 +1,33 @@
 $(document).ready(function() {
-    $('#id_visited_on_month').hide();
-    $('#id_visited_on_day').hide();
-    $('#id_visited_on_year').hide();
-    $('#id_my_rating').hide();
-    $('#id_top_ten').hide();
-    $('.helptext').hide();
+    var visitedCheckBox = document.getElementById('id_visited');
 
+    visitedCheckBox.addEventListener("click", respondVisitedCheck);
+
+    handleVisitedChecked(visitedCheckBox.checked);  // Initialize screen after refresh.
+});
+
+function respondVisitedCheck() {
+    var visited = document.getElementById('id_visited');
+
+    handleVisitedChecked(visited.checked)
+}
+
+function handleVisitedChecked(checked) {
     var els = document.getElementsByTagName('LABEL');
 
     for (var i = 0; i < els.length; i++) {
         if (els[i].innerHTML == "Visited on:" ||
             els[i].innerHTML == "My rating:" ||
             els[i].innerHTML == "Top ten:") {
-            els[i].style.display = "None";
+
+            els[i].style.display = (checked) ? "Block" : "None";
         }
     }
 
-    // Will also need function to listen for when visited gets checked and
-    // when it is checked to reverse what is done above and when unchecked to reverse
-    // again.
-});
+    (checked) ? $('#id_visited_on_month').show() : $('#id_visited_on_month').hide();
+    (checked) ? $('#id_visited_on_day').show() : $('#id_visited_on_day').hide();
+    (checked) ? $('#id_visited_on_year').show() : $('#id_visited_on_year').hide();
+    (checked) ? $('#id_my_rating').show() : $('#id_my_rating').hide();
+    (checked) ? $('#id_top_ten').show() : $('#id_top_ten').hide();
+    (checked) ? $('.helptext').show() : $('.helptext').hide();
+}
